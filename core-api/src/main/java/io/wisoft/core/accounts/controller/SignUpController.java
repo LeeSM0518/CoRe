e306f3f3f5170d901a9f259ff87eeb9616e4f023
Sangmin Lee <nalsm98@naver.com>
2020-09-13 20:40:07 +0900

    회원가입 컨트롤러 구현 완료

---
+++ core-api/src/main/java/io/wisoft/core/accounts/controller/SignUpController.java
@@ -0,0 +1,40 @@
+package io.wisoft.core.accounts.controller;
+
+import io.wisoft.core.accounts.dto.RequestToAuthenticateCode;
+import io.wisoft.core.accounts.dto.RequestToAuthenticateEmail;
+import io.wisoft.core.accounts.dto.RequestToSignUp;
+import io.wisoft.core.accounts.service.EmailAuthenticationService;
+import io.wisoft.core.accounts.service.SignUpService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/accounts")
+@RequiredArgsConstructor
+public class SignUpController {
+
+  private final EmailAuthenticationService emailAuthenticationService;
+  private final SignUpService signUpService;
+
+  @PostMapping("/signup/email")
+  public void requestCode(@Valid @RequestBody RequestToAuthenticateEmail dto, HttpSession session) {
+    emailAuthenticationService.requestAuthenticateEmail(session, dto.getEmail());
+  }
+
+  @PostMapping("/signup/code")
+  public void authenticateCode(@Valid @RequestBody RequestToAuthenticateCode dto, HttpSession session) {
+    emailAuthenticationService.requestAuthenticateCode(session, dto.getEmail(), dto.getCode());
+  }
+
+  @PostMapping("/signup")
+  public void signUp(@Valid @RequestBody RequestToSignUp dto) {
+    signUpService.signUp(dto.getEmail(), dto.getName(), dto.getPassword(), dto.getInterests());
+  }
+
+}
