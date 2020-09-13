2e7a1982edb5820b95828096319732e832f9a147
Sangmin Lee <nalsm98@naver.com>
2020-09-13 20:47:28 +0900

    예외 발생시 반환 클래스 구현완료

---
+++ core-api/src/main/java/io/wisoft/core/root/advice/ErrorResponse.java
@@ -0,0 +1,17 @@
+package io.wisoft.core.root.advice;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+
+  private String message;
+
+}
