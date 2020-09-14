e14fb3696633e20f87f64dc674df013b64363ded
Sangmin Lee <nalsm98@naver.com>
2020-09-14 20:33:34 +0900

    이메일 인증 & 회원가입 예외 Advice 업데이트

--- core-api/src/main/java/io/wisoft/core/accounts/advice/AuthenticationCodeExceptionAdvice.java
+++ core-api/src/main/java/io/wisoft/core/accounts/advice/AuthenticationCodeExceptionAdvice.java
@@ -1,7 +1,8 @@
 package io.wisoft.core.accounts.advice;
 
-import io.wisoft.core.accounts.exception.NotEqualCodeException;
-import io.wisoft.core.accounts.exception.NotFoundEmailInSessionException;
+import io.wisoft.core.accounts.exception.CodeNotEqualException;
+import io.wisoft.core.accounts.exception.EmailInSessionNotFoundException;
+import io.wisoft.core.accounts.exception.EmailNotAuthenticateException;
 import io.wisoft.core.root.advice.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,14 +12,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice("io.wisoft.core.accounts")
 public class AuthenticationCodeExceptionAdvice {
 
-  @ExceptionHandler(NotFoundEmailInSessionException.class)
+  @ExceptionHandler(EmailInSessionNotFoundException.class)
   public ResponseEntity<ErrorResponse> notFoundEmailInSession() {
     return ResponseEntity
         .status(HttpStatus.NOT_FOUND)
         .body(new ErrorResponse("인증 코드가 존재하지 않습니다. 인증 코드를 재요청해 주시길 바랍니다."));
   }
 
-  @ExceptionHandler(NotEqualCodeException.class)
+  @ExceptionHandler(CodeNotEqualException.class)
   public ResponseEntity<ErrorResponse> notEqualCode() {
     return ResponseEntity
         .status(HttpStatus.UNAUTHORIZED)
@@ -30,6 +31,13 @@ public class AuthenticationCodeExceptionAdvice {
     return ResponseEntity
         .status(HttpStatus.BAD_REQUEST)
         .body(new ErrorResponse("인증 코드는 반드시 숫자로 이루어져야 합니다."));
+  }
+
+  @ExceptionHandler(EmailNotAuthenticateException.class)
+  public ResponseEntity<ErrorResponse> emailNotAuthenticate() {
+    return ResponseEntity
+        .status(HttpStatus.UNAUTHORIZED)
+        .body(new ErrorResponse("인증되지 않은 이메일이거나 인증 시간 초과로 재인증을 받아야 합니다."));
   }
 
 }
--- core-api/src/main/java/io/wisoft/core/accounts/advice/SignUpExceptionAdvice.java
+++ core-api/src/main/java/io/wisoft/core/accounts/advice/SignUpExceptionAdvice.java
@@ -1,8 +1,8 @@
 package io.wisoft.core.accounts.advice;
 
-import io.wisoft.core.accounts.exception.DuplicateEmailException;
-import io.wisoft.core.accounts.exception.DuplicateNameException;
-import io.wisoft.core.accounts.exception.NotFoundInterestException;
+import io.wisoft.core.accounts.exception.EmailDuplicateException;
+import io.wisoft.core.accounts.exception.NameDuplicateException;
+import io.wisoft.core.accounts.exception.InterestNotFoundException;
 import io.wisoft.core.root.advice.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,21 +12,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice("io.wisoft.core.accounts")
 public class SignUpExceptionAdvice {
 
-  @ExceptionHandler(NotFoundInterestException.class)
+  @ExceptionHandler(InterestNotFoundException.class)
   public ResponseEntity<ErrorResponse> notFoundInterest() {
     return ResponseEntity
         .status(HttpStatus.BAD_REQUEST)
         .body(new ErrorResponse("관심사 중에 존재하지 않는 해시태그가 존재합니다. 관심사를 재등록하시길 바랍니다."));
   }
 
-  @ExceptionHandler(DuplicateEmailException.class)
+  @ExceptionHandler(EmailDuplicateException.class)
   public ResponseEntity<ErrorResponse> duplicateEmail() {
     return ResponseEntity
         .status(HttpStatus.CONFLICT)
         .body(new ErrorResponse("이메일이 이미 존재합니다."));
   }
 
-  @ExceptionHandler(DuplicateNameException.class)
+  @ExceptionHandler(NameDuplicateException.class)
   public ResponseEntity<ErrorResponse> duplicateName() {
     return ResponseEntity
         .status(HttpStatus.CONFLICT)
