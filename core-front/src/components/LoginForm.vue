daf4b245c0543b48e8e02cad7ee06b7e38709e06
Sangmin Lee <nalsm98@naver.com>
2020-09-16 22:39:33 +0900

    로그인 컴포넌트 구현중

---
+++ core-front/src/components/LoginForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-col cols="10">
+    <v-form v-model="valid">
+      <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        label="이메일"
+        dense
+        outlined
+      ></v-text-field>
+      <v-text-field
+        v-model="password"
+        :rules="passwordRules"
+        :type="show ? 'text' : 'password'"
+        @click:append="show = !show"
+        label="비밀번호"
+        dense
+        outlined
+      ></v-text-field>
+      <v-btn block color="primary" class="mb-5" :disabled="!valid"
+        ><b>로그인</b></v-btn
+      >
+    </v-form>
+  </v-col>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    show: false,
+    valid: true,
+    email: '',
+    emailRules: [
+      v => !!v || '이메일을 반드시 입력해주세요',
+      v => /.+@.+\..+/.test(v) || '이메일 형식에 맞춰서 입력해주세요',
+    ],
+    password: '',
+    passwordRules: [
+      v => !!v || '비밀번호를 반드시 입력해주세요',
+      v => v.length >= 8 || '최소 8 글자 이상 입력해주세요.',
+    ],
+  }),
+};
+</script>
+
+<style></style>
