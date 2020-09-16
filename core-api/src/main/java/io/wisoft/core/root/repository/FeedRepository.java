01a3d3e674c97ade24f037ed156a410ac8a09569
Sangmin Lee <nalsm98@naver.com>
2020-09-16 18:06:19 +0900

    FeedRepository 업데이트
    * 피드 조회 메서드 구현

--- core-api/src/main/java/io/wisoft/core/root/repository/FeedRepository.java
+++ core-api/src/main/java/io/wisoft/core/root/repository/FeedRepository.java
@@ -1,9 +1,19 @@
 package io.wisoft.core.root.repository;
 
 import io.wisoft.core.root.entity.Feed;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FeedRepository extends JpaRepository<Feed, Long> {
+
+  @Query("select f from Feed f join f.relatedTags r where r.name in :hashtags")
+  Page<Feed> findByHashtags(@Param("hashtags") List<String> hashtags, Pageable pageable);
+
 }
