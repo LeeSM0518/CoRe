4c4d9766418ad76e7654e87fe9905aec4121c57b
Sangmin Lee <nalsm98@naver.com>
2020-09-16 22:40:35 +0900

    로그인 & 회원가입 구현중

--- core-front/src/router/index.js
+++ core-front/src/router/index.js
@@ -1,23 +1,23 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
+    path: '/login',
+    name: 'Login',
+    components: {
+      header: null, // TODO 나중에 지우기
+      body: () => import('@/views/Login.vue'),
+    },
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/signup',
+    name: 'SignUp',
+    components: {
+      body: () => import('@/views/SignUp.vue'),
+    },
   },
 ];
 
