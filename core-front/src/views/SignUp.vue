7f8796d49b4298db6b2f0aec39aaeece35cdbc4d
Sangmin Lee <nalsm98@naver.com>
2020-09-16 22:40:13 +0900

    로그인 & 회원가입 구현중

---
+++ core-front/src/views/SignUp.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-row align="center" justify="center">
+    <v-card outlined width="400">
+      <v-row justify="center">
+        <v-img
+          src="../../public/img/main2.png"
+          max-width="348"
+          class="mr-3 mt-5"
+        />
+        <v-card-title class="text-subtitle-1 grey--text text--darken-1 mt-n5"
+          ><b>코드를 공유하고 리뷰하려면 가입해주세요.</b></v-card-title
+        >
+        <SignUpForm></SignUpForm>
+      </v-row>
+      <v-row class="mt-n5" justify="center">
+        <v-col cols="4">
+          <v-divider class="mt-3"></v-divider>
+        </v-col>
+        <v-col cols="1">
+          <v-row justify="center">
+            <b class="grey--text">또는</b>
+          </v-row>
+        </v-col>
+        <v-col cols="4">
+          <v-divider class="mt-3"></v-divider>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <router-link to="login" class="primary--text mb-2 text-button"
+          ><b>로그인</b></router-link
+        >
+      </v-row>
+    </v-card>
+  </v-row>
+</template>
+
+<script>
+import SignUpForm from '@/components/SignUpForm.vue';
+
+export default {
+  components: {
+    SignUpForm,
+  },
+};
+</script>
+
+<style></style>
