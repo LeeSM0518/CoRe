b4b4dffc066982ad18383a8cd1805d3da20651e7
Sangmin Lee <nalsm98@naver.com>
2020-09-18 00:26:44 +0900

    해시태그 예외 추가

---
+++ core-api/src/main/java/io/wisoft/core/hashtags/exception/HashtagNameContainsSpecialCharException.java
@@ -0,0 +1,4 @@
+package io.wisoft.core.hashtags.exception;
+
+public class HashtagNameContainsSpecialCharException extends RuntimeException {
+}
---
+++ core-api/src/main/java/io/wisoft/core/hashtags/exception/HashtagNameIsNullException.java
@@ -0,0 +1,4 @@
+package io.wisoft.core.hashtags.exception;
+
+public class HashtagNameIsNullException extends RuntimeException {
+}
